8448fa78156a1957f6d09464307a25a80d9cab2b
“liyu” <17806096859@163.com>
2021-09-27 11:27:35 +0800

    Add minimal home page

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
